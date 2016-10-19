4357cc7eaf9ed31da80af0730fb425321f3b4199
Jean Joeris <joeris.peter@gmail.com>
2016-10-19 10:38:14 -0600

    Final commit - done according to user story

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,5 +1,6 @@
 class NRELService
   def self.by_location(location)
+    # the next refactor I would look at is cleaning up this faraday request
     response = Faraday.get("https://api.data.gov/nrel/alt-fuel-stations/v1/nearest.json?limit=10&api_key=#{ENV['nrel_api_key']}&radius=6.0&location=80203&fuel_type=ELEC,LPG")
     data = JSON.parse(response.body, symbolize_names: true)
     data[:fuel_stations]
